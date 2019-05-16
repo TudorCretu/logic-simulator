1c9f21ec0327dcdfb673a0696b8cba8a14a8ed04
Shakthivel Ravichandran <caldays.r@gmail.com>
2019-05-16 12:27:00 +0100

    Initial Scanner skeleton

--- scanner.py
+++ scanner.py
@@ -8,7 +8,8 @@ Classes
 Scanner - reads definition file and translates characters into symbols.
 Symbol - encapsulates a symbol and stores its properties.
 """
-
+import os
+import sys
 
 class Symbol:
 
@@ -51,6 +52,80 @@ class Scanner:
 
     def __init__(self, path, names):
         """Open specified file and initialise reserved words and IDs."""
+        #if os.path.exists(path) :
+        self.file = open(path)
+        self.names = names
+        self.symbol_type_list = [self.BACKSLASH, self.COMMA, self.SEMICOLON,
+            self.EQUALS, self.KEYWORD, self.NUMBER, self.NAME, self.EOF] = range(8)
+        self.keywords_list = ["DEVICES", "CONNECTIONS", "MONITORS"]
+        [self.DEVICES_ID, self.CONNECT_ID, self.MONITOR_ID,
+            self.END_ID] = self.names.lookup(self.keywords_list)
+        self.current_character = ""
+
+
 
     def get_symbol(self):
         """Translate the next sequence of characters into a symbol."""
+        symbol = Symbol()
+        self.skip_spaces() # current character now not whitespace
+        if self.current_character.isalpha(): # name
+            name_string = self.get_name()
+            if name_string in self.keywords_list:
+                symbol.type = self.KEYWORD
+            else:
+                symbol.type = self.NAME
+                [symbol.id] = self.names.lookup([name_string])
+
+        elif self.current_character.isdigit():
+            symbol.id = self.get_number()
+            symbol.type = self.NUMBER
+
+        elif self.current_character == "=":
+            symbol.type = self.EQUALS
+            self.advance()
+
+        elif self.current_character == "/":
+            smybol.type = self.BACKSLASH
+            self.advance()
+
+        elif self.current_character == ",":
+            symbol.type = self.COMMA
+            self.advance()
+
+        elif self.current_character == ":":
+            symbol.type = self.SEMICOLON
+            self.advance()
+
+        elif self.current_character == "":
+            symbol.type = self.EOF
+
+        else:
+            self.advance()
+
+        return symbol 
+
+    def advance(self,input_file):
+        self.current_character = input_file.read(1)
+        return self.current_character
+
+
+    def skip_spaces(self):
+        while self.current_character.isspace():
+            self.advance()
+
+    def get_name(self):
+        name = ''
+        while self.current_character.isalnum() :
+            name += current_character
+            self.advance()
+        return name 
+        
+
+    def get_number(self):
+        number = ''
+
+        while self.current_character.isdigit() :
+            number += current_character
+            self.advance()
+
+        return number 
