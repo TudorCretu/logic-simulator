29a731555ed13fe55791a428c32d9bcb6a360cce
Dongcheng Jiang <dj346@cam.ac.uk>
2019-05-19 00:50:08 +0800

    test of name.py finished

---
+++ test_names.py
@@ -0,0 +1,32 @@
+import builtins
+import pytest
+import os
+from io import StringIO
+from names import Names
+
+def test_unique_error_codes():
+    n = Names()
+    assert n.unique_error_codes(6) == range(0,6)
+    assert n.error_code_count == 6
+    with pytest.raises(TypeError):
+        n.unique_error_codes(-1)
+        n.unique_error_codes("name")
+
+def test_query():
+    n = Names()
+    n.names = ["name1", "name2", "name3"]
+    assert n.query("name1") == 0
+    assert n.query("name4") is None
+
+def test_lookup():
+    """Test the look_up method"""
+    n = Names()
+    assert n.lookup(["name1"]) == [0]
+    assert n.lookup(["name2"])  == [1]
+    assert n.lookup(["name2","name3","name1"]) == [1,2,0]
+
+def test_get_name_string():
+    n = Names()
+    _ = n.lookup(["name1","name2","name3"])
+    assert n.get_name_string(1) == "name2"
+    assert n.get_name_string(5) is None
