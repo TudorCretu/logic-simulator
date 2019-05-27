a3297dd652c04cdefa1161f111ce6b5edbaef289
Shakthivel Ravichandran <caldays.r@gmail.com>
2019-05-27 14:47:16 +0100

    local test applied on mac device

--- parse.py
+++ parse.py
@@ -532,16 +532,17 @@ class Parser:
 #--------------------------------------local testing allowed-----------------------------------------------------------------------
 
 # Folder to keep test definition files
-test_file_dir = "test_functions"
+test_file_dir = "test_definition_files/test_monitors"
 names = Names()
 devices = Devices(names)
 network = Network(names, devices)
 monitors = Monitors(names, devices, network)
-file_path = test_file_dir + "/read_symbol.txt"
+file_path = test_file_dir + "/expected_name_error.txt"
 scanner = Scanner(file_path, names)
 parser = Parser(names, devices, network, monitors, scanner)
-a = parser.read_symbol()
-a = parser.read_symbol()
-print(parser.error_cursor)
+#a = parser.read_symbol()
+#a = parser.read_symbol()
+#print(parser.error_cursor)
 # print(parser.error_cursor[0]) # the cursor is None, msg captured right
+parser.parse_network()
 parser.print_msg(False)
