209111060a2dc15689c211b639e6e4b4316260e6
Shakthivel Ravichandran <caldays.r@gmail.com>
2019-05-28 17:11:52 +0100

    Commented out local testing of parse.py

--- parse.py
+++ parse.py
@@ -580,6 +580,7 @@ class Parser:
 
 #--------------------------------------local testing allowed-----------------------------------------------------------------------
 # Folder to keep test definition files
+'''
 test_file_dir = "test_definition_files/test_connections"
 names = Names()
 devices = Devices(names)
@@ -593,3 +594,4 @@ parser = Parser(names, devices, network, monitors, scanner)
 #print(parser.error_cursor)
 # print(parser.error_cursor[0]) # the cursor is None, msg captured right
 parser.parse_network()
+'''
