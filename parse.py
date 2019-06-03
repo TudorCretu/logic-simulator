e7fd140074f059d6b8e2a354215930a3b29963f3
Dongcheng Jiang <dj346@cam.ac.uk>
2019-06-03 09:36:33 +0100

    try colors

--- parse.py
+++ parse.py
@@ -93,31 +93,41 @@ class Parser:
         :return: no returned value.
         """
         if success is True:
-            print("Parsed successfully! Valid definition file!")
-            self.error_to_gui.append(
-                "Parsed successfully! Valid definition file!")
+            print('\033[91m'
+                  + "Parsed successfully! Valid definition file!"
+                  + '\033[0m')
+            self.error_to_gui.append('\033[91m'
+                                     + "Parsed successfully!"
+                                     "Valid definition file!"
+                                     + '\033[0m')
         else:
-            print("File '%s' contains %d error(s): "
-                  % (self.scanner.file.name.split('/')[-1], self.error_count))
-            self.error_to_gui.append("File '%s' contains %d error(s):"
+            print('\033[91m' + "File '%s' contains %d error(s): "
+                  % (self.scanner.file.name.split('/')[-1], self.error_count)
+                  + '\033[0m')
+            self.error_to_gui.append('\033[91m'
+                                     + "File '%s' contains %d error(s):"
                                      % (self.scanner.file.name.split('/')[-1],
-                                        self.error_count))
+                                        self.error_count) + '\033[0m')
             n = len(self.error_cursor)
             if self.error_count > n:  # notice the unconnected input error
                 for i in range(n):
-                    print(self.error_output[i])
-                    self.error_to_gui.append(self.error_output[i])
+                    print('\033[91m' + self.error_output[i] + '\033[0m')
+                    self.error_to_gui.append('\033[91m'
+                                             + self.error_output[i]
+                                             + '\033[0m')
                     # self.out_for_gui.append(self.error_output[i])
                     self.error_to_gui.append(self.scanner.show_error_location(
                         self.errline_num[i],
                         self.errline_pos[i],
                         self.error_cursor[i]))
-                print(self.error_output[n])
-                self.error_to_gui.append(self.error_output[n])
+                print('\033[91m' + self.error_output[n] + '\033[0m')
+                self.error_to_gui.append('\033[91m'
+                                         + self.error_output[n] + '\033[0m')
             else:
                 for i in range(n):
-                    print(self.error_output[i])
-                    self.error_to_gui.append(self.error_output[i])
+                    print('\033[91m' + self.error_output[i] + '\033[0m')
+                    self.error_to_gui.append('\033[91m'
+                                             + self.error_output[i] + '\033[0m')
                     self.error_to_gui.append(self.scanner.show_error_location(
                         self.errline_num[i],
                         self.errline_pos[i],
@@ -686,12 +696,15 @@ class Parser:
         :return: no returned value.
         """
         # go to the next comma within the section
+        # if self.symbol.type is None:
+        #     while self.symbol.type is None:
+        #         self.symbol = self.scanner.get_symbol()
+        # else:
         while self.symbol.type != self.scanner.COMMA:
             if self.symbol.type == self.scanner.KEYWORD \
                     or self.symbol.type == self.scanner.SEMICOLON \
                     or self.symbol.type == self.scanner.EOF:
                 return  # end of section or file, must terminate here
-            # self.symbol = self.read_symbol()
             self.symbol = self.scanner.get_symbol()
 
     def read_symbol(self):
@@ -699,34 +712,18 @@ class Parser:
         Read in the next symbol to be considered by the parser.
         If the scanner gets an invalid symbol, report syntax error,
             and then recover by moving on to get the next symbol.
-        :return: current_symbol, the next valid symbol for the parser.
+        :return: the next valid symbol for the parser.
         """
         # self.cursor = self.symbol.cursor_position
         self.symbol = self.scanner.get_symbol()
         if self.symbol.type is None:
             self.display_error(self.NOT_SYMBOL)
-            # self.skip_erratic_part()
             self.skip_erratic_part()
-        # print(self.symbol.cursor_position)
-        # while self.symbol.type is None:
-        #     self.display_error(self.NOT_SYMBOL)
-        #     self.symbol = self.scanner.get_symbol()
-        #     # print(self.symbol.cursor_position)
-        #     # print(current_symbol.type)
+
         return self.symbol
 
-    # def skip_erratic_symbol(self):  # recovery
-    #     """
-    #     This function is used for error recovery for invalid symbol.
-    #     It skips everything before finding the next valid symbol.
-    #     :return: no returned value.
-    #     """
-    #     while self.symbol.type is None:
-    #         self.symbol = self.scanner.get_symbol()
 
-
-
-# -------------------local testing #allowed------------------------
+# -------------------local testing allowed------------------------
 
 # # Folder to keep test definition files
 #
